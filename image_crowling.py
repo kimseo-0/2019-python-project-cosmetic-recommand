7c9ce9ee1a1721bc6e0cdf03798556a2f0020256
김서영 <rosa99kim@gmail.com>
2019-11-13 19:48:36 +0900

    Success : solve image crowling problem

--- image_crowling.py
+++ image_crowling.py
@@ -1,8 +1,9 @@
 import os
 import asyncio
 import re
-from pyppeteer import launch
+import time
 import urllib.request
+from pyppeteer import launch
 from bs4 import BeautifulSoup
 
 async def get_html_to_web(url) :
@@ -39,13 +40,14 @@ def get_attr_list_by_attrKey(base_html,attr) :
     '''
     return base_html[attr]
     
-def set_product_info(product_id, product_category,product_brand,product_name,product_url,product_color_name_list,product_image_src_list) :
+def set_product_info(product_id, product_ranking, product_category,product_brand,product_name,product_url,product_color_name_list,product_image_src_list) :
     '''
     make dict product category, brand, name, url, color list, image source list
     return  dict
     '''
     product_info = {
         "product_id" : product_id,
+        "product_ranking" : product_ranking,
         "product_category" : product_category, 
         "product_brand" : product_brand, 
         "product_name" : product_name, 
@@ -74,25 +76,29 @@ def create_forlder(directory) :
         print('Error:Creating directory' + directory)
 
 # 크롤링할 홈페이지 root url
-base_url = "https://www.powderroom.co.kr"+"/rankings/c3100"
+base_url = "https://www.powderroom.co.kr"
 
 # 크롤링 base_url+"/rankings/c3100"
-base_html = asyncio.get_event_loop().run_until_complete(get_html_to_web(base_url))
+base_html = asyncio.get_event_loop().run_until_complete(get_html_to_web(base_url+"/rankings/c3100"))
 base_html = BeautifulSoup(base_html,"html.parser")
 
 # 상품 타이틀 html list -> 각 상품 url quary list  -> 각 상품별 url list
 product_title_html_list = get_html_list_by_class(base_html, "card card-landscape product-ranking-card cursor-pointer")
 product_title_quary_list = list(map(lambda product_title_html : get_attr_list_by_attrKey(product_title_html,"np-href") , product_title_html_list))
-
+    
 # 모든 상품의 정보 저장
 # 모든 상품을 카테고리, 상품명 별로 각 색상별 상품 이미지 다운로드
+product_ranking = 0
 for product_title_quary in product_title_quary_list :
-
+    
+    # 상품 순위 - ranking 순대로 크롤링 되므로
+    product_ranking += 1
+    
     # 크롤링 url+"/variations"
     product_url = base_url + product_title_quary + "/variations" 
     product_html = asyncio.get_event_loop().run_until_complete(get_html_to_web(product_url))
     product_html = BeautifulSoup(product_html,"html.parser")
-    
+
     # 상품 정보 html
     product_info_html = get_html_by_class(product_html,"w-sm-70 w-60 card-body-large fl-col")
     
@@ -103,18 +109,22 @@ for product_title_quary in product_title_quary_list :
     # 상품 이미지 html list -> 상품 이미지 src list
     product_image_html_list =  get_html_list_by_class(product_html,"ar-content")
     product_image_src_list = list(map(lambda product_image_html :  get_attr_list_by_attrKey(product_image_html,"src") , product_image_html_list))
-
+   
     # 상품 정보 저장(카테고리, 브랜드, 상품명)
     product_category = get_html_by_class(product_info_html,"joint-dot").text
     product_brand = get_html_by_class(product_info_html,"card-text-secondary").text
     product_name = get_html_by_class(product_info_html,"card-text-primary").text 
 
-    p = re.compile('[0-9]+')
-    product_id = p.group(product_title_quary)
-    product_info = set_product_info(product_id, product_category,product_brand,product_name,product_url,product_color_name_list,product_image_src_list)
+    p = re.compile('[0-9]+') 
+    product_id = str((p.findall(product_title_quary))[0])
+    product_info = set_product_info(product_id, product_ranking, product_category,product_brand,product_name,product_url,product_color_name_list,product_image_src_list)
     
     create_forlder(os.getcwd()+"/image/"+product_category)
-    create_forlder(os.getcwd()+"/image/"+product_category+"/"+product_name+"("+product_id+")")
+    create_forlder(os.getcwd()+"/image/"+product_category+"/"+product_id)
 
     for i in range(len(product_color_name_list)) :
-        download_image_by_url(product_category+"/"+product_name+"("+product_id+")"+"/"+product_color_name_list[i],product_image_src_list[i+1]) 
+        download_image_by_url(product_category+"/"+product_id+"/"+product_color_name_list[i],product_image_src_list[i+1])
+    
+    print(product_info)
+    break
+    time.sleep(3)
